b8b60f4093d1addd1d4acf93b809d7b349ab5b84
komal patel <komal14march2002@gmail.com>
2023-11-03 16:00:52 +0530

    create register ass aemployee page

--- admin/register/confirm.php
+++ admin/register/confirm.php
@@ -1,29 +1,37 @@
 <link rel="shortcut icon" type="image/png" href="favicon.png"/>
 <?php
 session_start();
+
+require_once 'dompdf/autoload.inc.php'; 
+ 
+// Reference the Dompdf namespace 
+use Dompdf\Dompdf; 
+
 include 'connect.php';
 include 'mailer.php';
 ?>							
 <link rel="stylesheet" href="style.css">
 <title>::Leave Management::</title>
 <?php
-if(isset($_SESSION['adminuser']))
-{
-	$sql = "SELECT Dept,username FROM admins WHERE username = '".$_SESSION['adminuser']."'";
-	$result = $conn->query($sql);
-	if($result->num_rows > 0)
-		{
-			$row = $result->fetch_assoc();
-			$dept = $row["Dept"];
-		}
-include 'adminnavi.php';
+// // if(TRUE)
+// // {
+// 	$sql = "SELECT Dept,username FROM admins WHERE username = '".$_SESSION['adminuser']."'";
+// 	$result = $conn->query($sql);
+// 	if($result->num_rows > 0)
+// 		{
+// 			$row = $result->fetch_assoc();
+// 			$dept = $row["Dept"];
+// 		}
+include '../../navi.php';
 $errmsg = $sql = "";
 $empname = trim($_POST['empname']);
 $uname = trim($_POST['uname']);
+$dept = $_POST['dept'];
 $mailid = trim($_POST['mailid']);
 $doj = trim($_POST['year-join'])."-".trim($_POST['month-join'])."-".trim($_POST['date-join']);
 $dob = trim($_POST['year-birth'])."-".trim($_POST['month-birth'])."-".trim($_POST['date-birth']);
 $dob2 = trim($_POST['date-birth'])."-".trim($_POST['month-birth'])."-".trim($_POST['year-birth']);
+$gender = $_POST["gender"];
 $empname = strip_tags($empname);
 $uname = strip_tags($uname);
 $mailid = strip_tags($mailid);
@@ -74,7 +82,7 @@ if ((!filter_var($mailid, FILTER_VALIDATE_EMAIL)) || empty($mailid)) {
   $errmsg.="Invalid email ID...";
 	}
 }
-$sql2 = "SELECT * FROM admins WHERE username = '".$_SESSION['adminuser']."'";
+$sql2 = "SELECT * FROM admins WHERE Dept = '".$dept."'";
 if($conn->query($sql2) == TRUE)
 	{
 		$result = $conn->query($sql2);
@@ -96,7 +104,7 @@ else
 	{
 		echo "<div class = 'reg-form'>";
 		$pw = $uname;
-		$sql = "INSERT INTO employees (UserName,EmpPass,EmpName,Dept,EarnLeave,SickLeave,CasualLeave,EmpEmail,DateOfJoin,Designation,EmpType,EmpFee,DateOfBirth) VALUES "."('".$uname."','".$pw."','".$empname."','".$dept."','".$earnleave."','".$sickleave."','".$casualleave."','".$mailid."','".$doj."','".$designation."','".$emptype."','".$empfee."','".$dob."')";
+		$sql = "INSERT INTO joining_request (UserName,EmpPass,EmpName,Dept,EarnLeave,SickLeave,CasualLeave,EmpEmail,DateOfJoin,Designation,EmpType,EmpFee,DateOfBirth,gender) VALUES "."('".$uname."','".$pw."','".$empname."','".$dept."','".$earnleave."','".$sickleave."','".$casualleave."','".$mailid."','".$doj."','".$designation."','".$emptype."','".$empfee."','".$dob."','".$gender."')";
 		if ($conn->query($sql) === TRUE) {
 			echo "<center>";
 			echo "<strong> Registration Successful !</strong><br/><br/>";
@@ -119,16 +127,81 @@ else
 			echo "</center>";
 			echo "</div>";
 		}
-			else {
-				echo "Error: " . $sql . "<br>" . $conn->error;
-					}
-$conn->close();
+		else {
+			echo "Error: " . $sql . "<br>" . $conn->error;
+				}
+
+		$dompdf = new Dompdf();
+  
+		$pdf_content='
+		<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+		<html xmlns="http://www.w3.org/1999/xhtml">
+		<head>
+		<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+		</head>
+	
+		<style type="text/css">							
+			#pdf_header, #pdf_container{ border: 1px solid black; padding:10px; align : center;}				
+			#pdf_header{ margin:10px auto 0px; border-bottom:none; }				
+			table{width:100%;  border: 1px solid black; }				
+			#pdf_container{margin:0px auto; }
+			.rpt_title{ background:#99CCFF; }
+			tr{font-family: sans-serif;text-align: right;}
+			td{text-align:left;}
+			th{text-align: left; }
+			#engagement td,#engagement th{border-collapse: collapse; border:1px solid black; }															
+		</style>
+						
+		<body>
+		<div id="pdf_header" >
+		<table border="0" cellspacing="1" cellpadding="2">
+		<tr id="hdRow">
+			<td width="20%"><img src="GECR.jpg" height = 50 width = 50></td>				
+			<td width="30%" text-align="left">Government Engineering College<br/>Sejbahar, 492015</td>
+			</tr>
+		</table>
+		</div>
+		<div id="pdf_container" >
+		<table border="0" cellspacing="1" cellpadding="2">
+		<tr bgcolor="#3c4142" style="color:#FFF"><td colspan="3" align="left">Request for Joining GECR'.$empname.'</td></tr>
+			</table>
+		<table>
+		<tr><th>Employee Name : </th><td>'.$empname.'</td></tr>
+		<tr><th>Employee Designation : </th><td>'.$designation.'</td></tr>
+		<tr><th>Employment Type : </th><td>'.$emptype.'</td></tr>
+		<tr><th>Employee Department : </th><td>'.$dept.'</td></tr>
+		<tr><th>Type Of Leave : </th><td>'.$emptype.'</td></tr>
+		<tr><th>Employee Fee Structure : </th><td>'.$empfee.'</td></tr>
+		
+		<br><br><br>
+		</table>
+		</div></body></html>'
+		;
+		// $name = $user.$leavedate.$leavetype.$end.'.pdf';
+		// $reportPDF = createPDF($pdf_content, $name);
+
+		$dompdf->loadHtml($pdf_content); 
+		
+		// (Optional) Setup the paper size and orientation 
+		$dompdf->setPaper('A4', 'portrait'); 
+		
+		// Render the HTML as PDF 
+		$dompdf->render(); 
+		
+		// Output the generated PDF (1 = download and 0 = preview) 
+		// $dompdf->stream("codexworld", array("Attachment" => 0));
+		$name = "joining_request/".$uname.'_'.$mailid.'.pdf';
+		file_put_contents($name,$dompdf->output());
+		$conn->close();
+
 	}
-}
-else
-{
-	header('location:index.php?err='.urlencode('Please Login First To Access This Page !'));
-}
+// }
+// else
+// {
+// 	header('location:index.php?err='.urlencode('Please Login First To Access This Page !'));
+// }
+
+
 ?>
 <script type="text/javascript">
         function noBack()
---
+++ register_as_employee.php
@@ -0,0 +1,70 @@
+<link rel="shortcut icon" type="image/png" href="favicon.png"/>
+<?php
+session_start();
+?>
+<html>
+<head>
+<title>::Leave Management::</title>
+<link rel="stylesheet" href="admin/register/style.css">
+
+</head>
+<body>
+<div class="reg-form">
+<center>
+<h1>Leave Management System</h1>
+<?php
+include 'navi.php';?>
+<h2>New Employee Registration</h2>
+<i><div class = 'error'>*indicates mandatory fields</div></i>
+<?php
+if(isset($_GET['err']))
+	{
+		echo "<div class = 'error'><b><u>".htmlspecialchars($_GET['err'])."</u></b></div>";
+	}
+if(TRUE)
+	{
+	echo"<form id = 'myform' action = 'admin/register/confirm.php' method = 'post' style = 'border : 1px solid black;'>
+	<table style = 'border : 1px solid black; margin-top : 20px;'>
+	<tr><td><div class = 'error'>*</div> Employee Name : </td><td><input type = 'text' name = 'empname' class = 'reg-form-fields shadow selected' placeholder = 'Employee Name'></td></tr><br>
+<tr><td><div class = 'error'>*</div> Username : </td><td><input type = 'text' name = 'uname' class = 'reg-form-fields shadow selected' placeholder = 'Employee Username'></td></tr><br>
+<tr><td><div class = 'error'>*</div> Date of joining (dd/mm/yyyy): <td><input type = 'number' name = 'date-join' min = '1' max = '31' class = 'date-of-joining shadow selected' step = '1' placeholder = 'dd' style='width:50px;'><input type = 'number' name = 'month-join' min = '1' max = '12' class = 'date-of-joining shadow selected' step = '1' placeholder = 'mm' style='width:50px;'><input type = 'number' name = 'year-join' min = '1985' max = '".date('Y')."' class = 'date-of-joining shadow selected' step = '1' placeholder = 'yyyy' style='width:100px;'></td></tr><br>
+<tr><td><div class = 'error'>*</div> Date of birth (dd/mm/yyyy): <td><input type = 'number' name = 'date-birth' min = '1' max = '31' class = 'date-of-joining shadow selected' step = '1' placeholder = 'dd' style='width:50px;'><input type = 'number' name = 'month-birth' min = '1' max = '12' class = 'date-of-joining shadow selected' step = '1' placeholder = 'mm' style='width:50px;'><input type = 'number' name = 'year-birth' min = '1901' max = '".date('Y')."' class = 'date-of-joining shadow selected' step = '1' placeholder = 'yyyy' style='width:100px;'></td></tr><br>
+<tr><td><div class = 'error'>*</div> Employee email id : </td><td><input type = 'text' name = 'mailid' class = 'reg-form-fields shadow selected' placeholder = 'Employee Email ID'></td></tr><br>
+<tr><td><div class = 'error'>*</div> Department : </td><td><select name = 'dept' class = 'reg-form-fields shadow selected'>
+						<option>CSE</option><option>ET&T</option><option>MECH</option><option>CIVIL</option><option>EEE</option>
+				  </select>
+				  </td></tr><br>
+<tr><td><div class = 'error'>*</div> Gender : </td><td><select name = 'gender' class = 'reg-form-fields shadow selected'>
+						<option>Male</option><option>Female</option>
+				  </select>
+				  </td></tr><br>
+<tr><td><div class = 'error'>*</div> Designation : </td><td><input type = 'text' name = 'designation' class = 'reg-form-fields shadow selected' placeholder = 'Employee Designation'></td></tr><br>
+<tr><td><div class = 'error'>*</div> Employment Type : </td><td><select name = 'factype' class = 'reg-form-fields shadow selected'>
+						<option>Permanent</option><option>Ad-hoc</option><option>Fix</option>
+				  </select>
+				  <select name = 'facfee' class = 'reg-form-fields shadow selected'>
+						<option>Grant In Aid</option><option>Self Finance</option>
+				  </select></td></tr><br>
+<tr><td><input type = 'submit' value = 'Submit' class = 'registration shadow'></td></tr>
+</form>
+</table>
+</center>
+</div>";
+	}
+	else
+	{
+		header('location:index.php?err='.urlencode('Please Login First To Access This Page !'));
+	}
+
+	echo "<script>
+			const form = document.getElementById('myform');
+			form.addEventListener('keypress',function(e){
+				if(e.keyCode === 13){
+					e.preventDefault();
+
+				}
+			});
+		</script>"
+?>
+</body>
+</html>
